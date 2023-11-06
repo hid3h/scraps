6e75de80466ef0b0f2a98cfc339b37122cd39fd4
hid3h <razyetts@hotmail.co.jp>
2023-11-06 22:00:58 +0900

    デザインこれで

--- app/scraps/[id]/CommentCardMenu.tsx
+++ app/scraps/[id]/CommentCardMenu.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { Menu, Transition } from "@headlessui/react";
-import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  EllipsisVerticalIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 import { Fragment } from "react";
 
 function classNames(...classes: string[]) {
@@ -10,36 +14,42 @@ function classNames(...classes: string[]) {
 
 export default function CommentCardMenu() {
   return (
-    <Menu as="div" className="relative ml-3 inline-block text-left">
+    <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="-my-2 flex items-center rounded-full bg-white p-2 text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500">
-          <span className="sr-only">Open options</span>
-          <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+        <Menu.Button className="inline-flex w-full justify-center rounded-full bg-white px-2 py-2 hover:bg-gray-50">
+          <ChevronDownIcon
+            className="h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
         </Menu.Button>
       </div>
 
       <Transition
         as={Fragment}
-        enter="transition ease-out"
+        enter="transition ease-out duration-100"
         enterFrom="transform opacity-0 scale-95"
         enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in"
+        leave="transition ease-in duration-75"
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
-                <button
-                  type="button"
+                <a
+                  href="#"
                   className={classNames(
-                    active ? "bg-red-50 text-red-500" : "text-red-500",
-                    "flex w-full justify-between px-4 py-2 text-sm"
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "group flex items-center px-4 py-2 text-sm"
                   )}
                 >
-                  <span>削除</span>
-                </button>
+                  <TrashIcon
+                    className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                    aria-hidden="true"
+                  />
+                  Delete
+                </a>
               )}
             </Menu.Item>
           </div>
