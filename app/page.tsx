dca5268f3da80b919a1b1a2a0ab230b14c1812aa
hid3h <razyetts@hotmail.co.jp>
2023-11-06 21:38:33 +0900

    トップページにコンテンツないのでログインしてたらダッシュボードにリダイレクトした方がいいかなと

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Link from "next/link";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await auth();
+  if (session?.user) {
+    return redirect("/dashboard");
+  }
   return;
 }
