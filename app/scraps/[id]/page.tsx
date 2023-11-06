f09fa7b05ea89c49f66dc3e3bcdc9a737056e977
hid3h <razyetts@hotmail.co.jp>
2023-11-06 21:36:00 +0900

    コメントを削除できるようにしようとしている

--- app/scraps/[id]/page.tsx
+++ app/scraps/[id]/page.tsx
@@ -4,6 +4,7 @@ import { AddScrapCommentForm } from "./add-scrap-comment-form";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
+import CommentCardMenu from "./CommentCardMenu";
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
   const scrap = await findScrap({
@@ -28,18 +29,21 @@ export default async function Scrap({ params }: { params: { id: string } }) {
     <div className="flex justify-center">
       <div className="flex-1 max-w-screen-sm">
         <ScrapHeading scrap={scrap} />
-        <ul role="list" className="space-y-3">
+        <div role="list" className="space-y-3">
           {scrap.scrapCommentings.map((scrapComment) => (
-            <li
+            <div
               key={scrapComment.id}
               className="overflow-hidden bg-white py-4 shadow rounded-md px-6"
             >
+              <div className="flex flex-shrink-0 justify-start">
+                <CommentCardMenu />
+              </div>
               <Markdown remarkPlugins={[remarkGfm, remarkBreaks]}>
                 {displayComment(scrapComment.body)}
               </Markdown>
-            </li>
+            </div>
           ))}
-        </ul>
+        </div>
         <AddScrapCommentForm scrap={scrap} />
       </div>
     </div>
