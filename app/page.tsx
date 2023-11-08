5e7a3077c68c6d647aa70e4537f8c2f444254eb7
hid3h <razyetts@hotmail.co.jp>
2023-11-08 19:32:02 +0900

    adsense用にトップにコンテンツ用意

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import { auth } from "@/auth";
+import prisma from "@/db";
+import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -6,5 +9,43 @@ export default async function Home() {
   if (session?.user) {
     return redirect("/dashboard");
   }
-  return;
+
+  const scraps = await prisma.scrapPosting.findMany({
+    include: {
+      scrapCommentings: true,
+    },
+  });
+
+  return (
+    <>
+      <ul role="list" className="divide-y divide-gray-100">
+        {scraps.map((scrap) => (
+          <li key={scrap.id} className="py-5">
+            <p className="font-semibold leading-6 text-gray-900">
+              <Link href={"/scraps/" + scrap.id} className="hover:underline">
+                {scrap.title}
+              </Link>
+            </p>
+            <div className="mt-1 flex items-center justify-between text-xs leading-5 text-gray-500">
+              <p>
+                <time>{scrap.postedAt.toString()}</time>
+              </p>
+              <dl className="">
+                <div className="flex w-16 gap-x-2.5">
+                  <dt>
+                    <span className="sr-only">Total comments</span>
+                    <ChatBubbleLeftIcon
+                      className="h-6 w-6 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </dt>
+                  <dd className="text-sm leading-6 text-gray-900">1</dd>
+                </div>
+              </dl>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
