2733c60bc5c2a116e98ac7c1038b02decafc0071
hid3h <razyetts@hotmail.co.jp>
2023-11-10 12:39:09 +0900

    スクラップがまだ作成されていなとき

--- app/_components/ScrapSummary.tsx
+++ app/_components/ScrapSummary.tsx
@@ -12,35 +12,43 @@ export const ScrapSummary = ({
   }[];
 }) => {
   return (
-    <ul role="list" className="divide-y divide-gray-100">
-      {scraps.map((scrap) => (
-        <li key={scrap.id} className="py-5">
-          <p className="font-semibold leading-6 text-gray-900">
-            <Link href={"/scraps/" + scrap.id} className="hover:underline">
-              {scrap.title}
-            </Link>
-          </p>
-          <div className="mt-1 flex items-center justify-between text-xs leading-5 text-gray-500">
-            <p>
-              <time dateTime={scrap.postedAt}>{scrap.postedAt}</time>
-            </p>
-            <dl className="">
-              <div className="flex w-16 gap-x-2.5">
-                <dt>
-                  <span className="sr-only">Total comments</span>
-                  <ChatBubbleLeftIcon
-                    className="h-6 w-6 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </dt>
-                <dd className="text-sm leading-6 text-gray-900">
-                  {scrap.commentCount}
-                </dd>
+    <>
+      {scraps.length === 0 ? (
+        <p className="text-center text-gray-500">
+          スクラップはまだ作成されていません
+        </p>
+      ) : (
+        <ul role="list" className="divide-y divide-gray-100">
+          {scraps.map((scrap) => (
+            <li key={scrap.id} className="py-5">
+              <p className="font-semibold leading-6 text-gray-900">
+                <Link href={"/scraps/" + scrap.id} className="hover:underline">
+                  {scrap.title}
+                </Link>
+              </p>
+              <div className="mt-1 flex items-center justify-between text-xs leading-5 text-gray-500">
+                <p>
+                  <time dateTime={scrap.postedAt}>{scrap.postedAt}</time>
+                </p>
+                <dl className="">
+                  <div className="flex w-16 gap-x-2.5">
+                    <dt>
+                      <span className="sr-only">Total comments</span>
+                      <ChatBubbleLeftIcon
+                        className="h-6 w-6 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </dt>
+                    <dd className="text-sm leading-6 text-gray-900">
+                      {scrap.commentCount}
+                    </dd>
+                  </div>
+                </dl>
               </div>
-            </dl>
-          </div>
-        </li>
-      ))}
-    </ul>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
