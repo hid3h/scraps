29167dee3455bb39c6c4a70cfa03e678feeda634
hid3h <razyetts@hotmail.co.jp>
2023-11-16 16:10:05 +0900

    画像不要

--- app/page.tsx
+++ app/page.tsx
@@ -44,9 +44,6 @@ export default async function Home() {
               </a>
             </div>
           </div>
-          <div className="mt-16 flow-root sm:mt-24">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"></div>
-          </div>
         </div>
       </div>
       <div
